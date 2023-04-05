9500745ec412fba9cf61f549a67c9558ff17e7b1
Thomas REMY <contact.t.remy777@gmail.com>
2023-04-05 09:50:05 +0200

    fix(wrapper): add missing return type

--- app/Model/Wrapper/OpenLibrary.php
+++ app/Model/Wrapper/OpenLibrary.php
@@ -5,7 +5,9 @@ namespace Model\Wrapper;
 
 require 'vendor/autoload.php';
 
+use Google\Service\PeopleService\Resource\People;
 use GuzzleHttp\Client;
+use GuzzleHttp\Promise\PromiseInterface;
 
 class OpenLibrary extends AbstractWrapper {
     private Client $httpClient;
@@ -15,27 +17,27 @@ class OpenLibrary extends AbstractWrapper {
         $this->httpClient = $this->getHttpClient();
     }
 
-    public function getWorkByOLID(string $olid) {
+    public function getWorkByOLID(string $olid): PromiseInterface {
         return $this->httpClient->getAsync('/works/' . $olid . '.json');
     }
 
-    public function getEditionByOLID(string $olid) {
+    public function getEditionByOLID(string $olid): PromiseInterface {
         return $this->httpClient->getAsync('/books/' . $olid . '.json');
     }
 
-    public function getEditionByISBN(string $isbn) {
+    public function getEditionByISBN(string $isbn): PromiseInterface {
         return $this->httpClient->getAsync('/isbn/' . $isbn . '.json');
     }
 
-    public function getAuthorByOLID(string $olid) {
+    public function getAuthorByOLID(string $olid): PromiseInterface {
         return $this->httpClient->getAsync('/authors/' . $olid . '.json');
     }
 
-    public function getAuthorsWorks(string $authorsOLID, int $limit = 50) {
+    public function getAuthorsWorks(string $authorsOLID, int $limit = 50): PromiseInterface {
         return $this->httpClient->requestAsync('GET', '/authors/' . $authorsOLID . '/works.json', ['query'=>['limit'=>strval($limit)]]);
     }
 
-    public function getWorksBySubject(string $subject) {
+    public function getWorksBySubject(string $subject): PromiseInterface {
         return $this->httpClient->getAsync('/subjects/' . urlencode($subject) . '.json');
     }
 }
