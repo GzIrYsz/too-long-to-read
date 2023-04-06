e160a854856848a123fa47e0fdec1899d9a7ba1d
Thomas REMY <contact.t.remy777@gmail.com>
2023-04-06 17:37:03 +0200

    TLTR-13 feat: improve wrapper

--- app/Model/Wrapper/Nasa.php
+++ app/Model/Wrapper/Nasa.php
@@ -21,4 +21,16 @@ class Nasa extends AbstractWrapper {
     public function getApod(string $date = null, string $startDate = null, string $endDate = null): PromiseInterface {
         return $this->httpClient->getAsync('/planetary/apod', ['query'=>['api_key'=>$this->token]]);
     }
+
+    public function getSpecificApod(string $date): PromiseInterface {
+        return $this->httpClient->getAsync('/planetary/apod', ['query'=>['api_key'=>$this->token, 'date'=>$date]]);
+    }
+
+    public function getRangedApod(string $startDate, string $endDate): PromiseInterface {
+        return $this->httpClient->getAsync('/planetary/apod', ['query'=>['api_key'=>$this->token, 'start_date'=>$startDate, 'end_date'=>$endDate]]);
+    }
+
+    public function getNApod(int $n): PromiseInterface {
+        return $this->httpClient->getAsync('/planetary/apod', ['query'=>['api_key'=>$this->token, 'count'=>strval($n)]]);
+    }
 }
