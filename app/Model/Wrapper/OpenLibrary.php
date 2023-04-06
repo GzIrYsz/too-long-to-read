ddf849e96982fd9ede5b6faba72f1b1d94160e10
Thomas REMY <contact.t.remy777@gmail.com>
2023-04-06 15:19:25 +0200

    TLTR-11 fix: remove unwanted attribute

--- app/Model/Wrapper/OpenLibrary.php
+++ app/Model/Wrapper/OpenLibrary.php
@@ -9,34 +9,31 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Promise\PromiseInterface;
 
 class OpenLibrary extends AbstractWrapper {
-    private Client $httpClient;
-
     public function __construct() {
         parent::__construct('https://openlibrary.org');
-        $this->httpClient = $this->getHttpClient();
     }
 
     public function getWorkByOLID(string $olid): PromiseInterface {
-        return $this->httpClient->getAsync('/works/' . $olid . '.json');
+        return $this->getHttpClient()->getAsync('/works/' . $olid . '.json');
     }
 
     public function getEditionByOLID(string $olid): PromiseInterface {
-        return $this->httpClient->getAsync('/books/' . $olid . '.json');
+        return $this->getHttpClient()->getAsync('/books/' . $olid . '.json');
     }
 
     public function getEditionByISBN(string $isbn): PromiseInterface {
-        return $this->httpClient->getAsync('/isbn/' . $isbn . '.json');
+        return $this->getHttpClient()->getAsync('/isbn/' . $isbn . '.json');
     }
 
     public function getAuthorByOLID(string $olid): PromiseInterface {
-        return $this->httpClient->getAsync('/authors/' . $olid . '.json');
+        return $this->getHttpClient()->getAsync('/authors/' . $olid . '.json');
     }
 
     public function getAuthorsWorks(string $authorsOLID, int $limit = 50): PromiseInterface {
-        return $this->httpClient->requestAsync('GET', '/authors/' . $authorsOLID . '/works.json', ['query'=>['limit'=>strval($limit)]]);
+        return $this->getHttpClient()->requestAsync('GET', '/authors/' . $authorsOLID . '/works.json', ['query'=>['limit'=>strval($limit)]]);
     }
 
     public function getWorksBySubject(string $subject): PromiseInterface {
-        return $this->httpClient->getAsync('/subjects/' . urlencode($subject) . '.json');
+        return $this->getHttpClient()->getAsync('/subjects/' . urlencode($subject) . '.json');
     }
 }
