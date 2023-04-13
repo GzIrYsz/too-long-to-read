a6307d5975a7c7929f3962423200201fcdf5531b
Thomas REMY <contact.t.remy777@gmail.com>
2023-04-13 18:29:25 +0200

    fix: remove bug in controller

--- app/Controller/TeamController.php
+++ app/Controller/TeamController.php
@@ -5,15 +5,13 @@ namespace App\Controller;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-class TeamController extends \Core\Controller\AbstractController
-{
+class TeamController extends \Core\Controller\AbstractController {
 
     public function index(Request $req, Response $res, array $args): Response {
         $author = 'Thomas REMY';
         $description = 'description';
         $keywords = 'a, b, c';
         $title = 'Test Page';
-        $req->getQuery
         $res->getBody()->write($this->render('team', compact('author', 'description', 'keywords', 'title')));
         return $res;
     }
